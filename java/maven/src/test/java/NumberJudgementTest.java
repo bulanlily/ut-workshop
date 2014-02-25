bf07c4d0cc956ae3fe87eb0763b6dbd277504b0d
Li Yuan <yuanli@thoughtworks.com>
2014-02-25 10:48:24 +0800

    separate the test case

--- java/maven/src/test/java/NumberJudgementTest.java
+++ java/maven/src/test/java/NumberJudgementTest.java
@@ -4,7 +4,7 @@ import static org.fest.assertions.api.Assertions.assertThat;
 
 public class NumberJudgementTest {
     @Test
-    public void should_judge_input_match_number() {
+    public void should_judge_input_match() {
         NumberJudgement numberJudgement = new NumberJudgement("1234");
 
         assertThat(numberJudgement.judgeA("1253")).isEqualTo(2);
@@ -18,11 +18,21 @@ public class NumberJudgementTest {
     }
 
     @Test
-    public void should_judge_input_match_number_and_position() {
+    public void should_judge_4A0B_match_number_and_position() {
         NumberJudgement numberJudgement = new NumberJudgement("1234");
 
         assertThat(numberJudgement.judgeResult("1234")).isEqualTo("4A0B");
+    }
+    @Test
+    public void should_judge_0A0B_match_number_and_position() {
+        NumberJudgement numberJudgement = new NumberJudgement("1234");
+
         assertThat(numberJudgement.judgeResult("5678")).isEqualTo("0A0B");
+    }
+    @Test
+    public void should_judge_2A1B_match_number_and_position() {
+        NumberJudgement numberJudgement = new NumberJudgement("1234");
+
         assertThat(numberJudgement.judgeResult("1253")).isEqualTo("2A1B");
     }
 
